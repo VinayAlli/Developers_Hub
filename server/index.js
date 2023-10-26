3483b73aefa89c0c0284684f398fbd802ee18969
VinayAlli <71221248+VinayAlli@users.noreply.github.com>
2023-10-26 11:58:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 const app=express()
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://developers-hub-one.vercel.app'); // You can also use '*' to allow any origin.
+  res.header('Access-Control-Allow-Origin', '*'); // You can also use '*' to allow any origin.
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
